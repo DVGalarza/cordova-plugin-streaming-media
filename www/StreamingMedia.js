e1e7bfe552c14ca7e4ad5f8ab0b5b11ab82b6e43
sandboxcastles <nicklandkamer@gmail.com>
2022-12-11 10:24:33 -0600

    add media id

--- www/StreamingMedia.js
+++ www/StreamingMedia.js
@@ -1,29 +1,37 @@
 "use strict";
 function StreamingMedia() {
+	this.id = cordova.utils.createUUID();
+}
+
+function getOptions(streamingMedia, srcOptions) {
+	return {
+		...srcOptions,
+		id: streamingMedia.id
+	};
 }
 
 StreamingMedia.prototype.playAudio = function (url, options) {
-	options = options || {};
+	options = getOptions(this, options);
 	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playAudio", [url, options]);
 };
 
 StreamingMedia.prototype.pauseAudio = function (options) {
-    options = options || {};
+	options = getOptions(this, options);
     cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "pauseAudio", [options]);
 };
 
 StreamingMedia.prototype.resumeAudio = function (options) {
-    options = options || {};
+	options = getOptions(this, options);
     cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "resumeAudio", [options]);
 };
 
 StreamingMedia.prototype.stopAudio = function (options) {
-    options = options || {};
+	options = getOptions(this, options);
     cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "stopAudio", [options]);
 };
 
 StreamingMedia.prototype.playVideo = function (url, options) {
-	options = options || {};
+	options = getOptions(this, options);
 	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playVideo", [url, options]);
 };
 
